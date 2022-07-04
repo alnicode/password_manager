be656f5165cb8dc7db552d1e0c9060ba62b00946
Alben Bustamante <albenbustamante@gmail.com>
2022-07-04 00:46:10 -0500

    Initial commit

---
+++ src/main/java/com/alnicode/password/manager/PasswordManager.java
@@ -0,0 +1,16 @@
+package com.alnicode.password.manager;
+
+/**
+ * I'm open to feedback :D
+ *
+ * @author Alben Bustamante
+ * @since 1.0
+ * @version 1.0
+ */
+public class PasswordManager {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+    
+}
